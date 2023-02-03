f86d28a72afd4946d041c7b6a1785dab842f7db0
victor-okereke <okerekev324@gmail.com>
2023-02-02 18:21:03 -0800

    updates

--- middlewares/authorization/user.auth.ts
+++ middlewares/authorization/user.auth.ts
@@ -1,3 +1,4 @@
+import { NextFunction } from "express";
 import jwt, { JwtPayload, Secret } from "jsonwebtoken";
 
 interface payload extends JwtPayload {
@@ -9,3 +10,9 @@ const secret = "rrneitnijtnntyhdvatywqopqwi";
 export const generateToken = (user: payload) => {
   return jwt.sign(user, secret as Secret, { expiresIn: "1h" });
 };
+
+// const userAuth = (req: Request, res: Response, next: NextFunction) => {
+//   const headers = req.headers.authorization;
+//   if (!headers || !headers.startsWith('Bearer '))
+
+// }
